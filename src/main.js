0720c7a45e7e3ee84c89d2f45385037abe92f407
risachoi64 <risa.choi64@gmail.com>
2023-03-24 14:15:26 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import globalDirectives from './plugins/global-directives';
+import dayjs from './plugins/dayjs';
+import person from './plugins/person';
+// import focus from '@/directives/focus';
+// import globalComponents from '@/plugins/global-components';
+
+const app = createApp(App);
+// app.use(globalComponents);
+// app.directive('focus', focus);
+app.use(router);
+app.use(person);
+app.use(globalDirectives);
+app.use(dayjs);
+app.mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
+
+// console.log('MODE: ', import.meta.env.MODE);
+// console.log('BASE_URL: ', import.meta.env.BASE_URL);
+// console.log('PROD: ', import.meta.env.PROD);
+// console.log('DEV: ', import.meta.env.DEV);
+// console.log('VITE_APP_API_URL: ', import.meta.env.VITE_APP_API_URL);
