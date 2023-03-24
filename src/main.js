09cc91b5253876eda5f71df49d3bee9af1e1c996
risachoi64 <risa.choi64@gmail.com>
2023-03-24 17:02:06 +0900

    0324

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './assets/main.css'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
